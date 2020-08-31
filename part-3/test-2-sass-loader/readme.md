9130f068dc653469fa3916b824c4caf112c72763
Quy <tranphuquy19@gmail.com>
2020-08-31 23:08:17 +0700

    test 2 SASS loader - scss

---
+++ part-3/test-2-sass-loader/readme.md
@@ -0,0 +1,5 @@
+# SASS loader
+
++ sass-loader + node-sass: chuyển đỗi sass -> css
++ css-loader: chuyển đổi css thành JS module
++ style-loader: chèn css module vào thể <style></style> của trang
---
+++ part-3/test-2-sass-loader/src/index.js
@@ -0,0 +1,11 @@
+import {Logger} from './libs/logger';
+import turtle from './assets/turtle.jpg';
+import style from './style.scss';
+
+Logger('Live reload successful ahihi');
+
+document.body.innerHTML = '<div id="myAvatar"></div>';
+document.getElementById("myAvatar").innerHTML = `
+  <h1>And his name is...</h1>
+  <img src="${turtle}"/>
+`;
